2d055256a2eaaafe3d04425afbe776092f570628
kobso1245 <kobso1245@gmail.com>
2014-10-31 17:21:28 +0200

    test

--- RacketProj/zad1_domashno4.scm
+++
@@ -1,9 +0,0 @@
-(define (test)
-  ((lambda (x) (* x x)) 3))
-(define (less-than list)
-  (lambda (x)
-    (if (null? list ) '()
-               (if (> x (car list)) (cons (car list) ((less-than (cdr list)) x))
-                   ((less-than (cdr list)) x)))))
-
-                                                
