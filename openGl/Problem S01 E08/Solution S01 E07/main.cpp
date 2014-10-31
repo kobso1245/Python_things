681f41a22a473b86cf3795f83ca765361397bf3c
kobso1245 <kobso1245@gmail.com>
2014-10-31 17:04:55 +0200

    try

---
+++ openGl/Problem S01 E08/Solution S01 E07/main.cpp
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <GL/glx.h>    /* this includes the necessary X headers */
+#include <GL/gl.h>
+
+#include <X11/X.h>    /* X11 constant (e.g. TrueColor) */
+#include <X11/keysym.h>
+
+int main()
+{
+    int     width, height;
+    int     frame = 0;
+    bool    running = true;
+
+    //
+    // Инициализация на GLFW
+    //
+    glfwInit();
+
+    //
+    // Инициализация и отваряне на графичен позорец
+    //
+    if( !glfwOpenWindow( 512, 512, 0, 0, 0, 0, 0, 0, GLFW_WINDOW ) )
+    {
+        glfwTerminate();
+        return 0;
+    }
+
+    glfwSetWindowTitle("GLFW Application");
+
+    //
+    // Основен цикъл на анимацията за
+    // генериране на кадър по кадър
+    //
+    while(running)
+    {
+        frame++;
+
+        //
+        // Изчистване на работните буфери
+        //
+        glfwGetWindowSize( &width, &height );
+        height = height > 0 ? height : 1;
+
+        glViewport( 0, 0, width, height );
+
+        glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+        glClear( GL_COLOR_BUFFER_BIT );
+
+        //
+        // Установяване на вида на проекция
+        //
+        glMatrixMode( GL_PROJECTION );
+        glLoadIdentity();
+        gluPerspective( 65.0f, (GLfloat)width/(GLfloat)height, 1.0f, 100.0f );
+
+        //
+        // Установяване на гледната точка
+        //
+        glMatrixMode( GL_MODELVIEW );
+        glLoadIdentity();
+        gluLookAt(0.0f, -10.0f, 0.0f,
+                0.0f, 0.0f, 0.0f,
+                0.0f, 0.0f, 1.0f );
+
+        //
+        // Рисуване на един кадър в скрит буфер
+        //
+        glRotatef(frame, 0.25f, 1.0f, 0.75f);
+        glBegin( GL_TRIANGLES );
+          glColor3f(0.1f, 0.0f, 0.0f );
+          glVertex3f(0.0f, 3.0f, -4.0f);
+          glColor3f(0.0f, 1.0f, 0.0f );
+          glVertex3f(3.0f, -2.0f, -4.0f);
+          glColor3f(0.0f, 0.0f, 1.0f );
+          glVertex3f(-3.0f, -2.0f, -4.0f);
+        glEnd();
+        glBegin( GL_TRIANGLES );
+          glColor3f(0.0f, 0.1f, 0.0f );
+          glVertex3f(0.0f, 3.0f, -3.0f);
+          glColor3f(0.0f, 0.0f, 1.0f );
+          glVertex3f(3.0f, -2.0f, -2.0f);
+          glColor3f(1.0f, 0.0f, 0.0f );
+          glVertex3f(-3.0f, -2.0f, 2.0f);
+        glEnd();
+
+        //
+        // Подмяна на скрития буфер с видимия
+        //
+        glfwSwapBuffers();
+
+        // exit if ESC was pressed or window was closed
+        running = !glfwGetKey(GLFW_KEY_ESC) && glfwGetWindowParam( GLFW_OPENED);
+    }
+
+    //
+    // Приключване на работата
+    //
+    glfwTerminate();
+
+    return 0;
+}
---
+++ openGl/glfw-2.7.bin.WIN32/README.txt
@@ -0,0 +1,26 @@
+About this package
+==================
+
+This is the Win32 binary package of GLFW 2.7.  It contains static, dynamic and
+link libraries built with and using the runtimes of a number of common Windows
+compilers.
+
+More specifically, it contains files for the following compilers:
+
+ * Cygwin 1.7
+ * MinGW 4.5.0
+ * Visual C++ 2008 (debug and release runtimes)
+ * Visual C++ 2010 (debug and release runtimes)
+
+About GLFW
+==========
+
+GLFW is a portable framework for OpenGL application development. It handles
+operating system specific tasks such OpenGL window management, resolution
+switching, keyboard, mouse and joystick input, timer input, creating threads,
+and more. 
+
+The website of GLFW is: http://www.glfw.org/
+
+For license information, see the COPYING.txt file.
+
